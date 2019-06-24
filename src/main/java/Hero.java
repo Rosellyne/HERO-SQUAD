8c3e892690887f7489163c9d74723f62603c952a
Rosellyne <elijahroselyne@gmail.com>
2019-06-24 14:43:40 +0300

    added the tests to accept the name,age,specialp powers and weakness of the hero

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -8,7 +8,32 @@ public class Hero {
     private String mWeakness;
     private static List<Hero> instances = new ArrayList<>();
     private int mId;
-    public Hero(String name,int age,String special_power,String weakness){
+
+    public String getmName() {
+        return mName;
+    }
+
+    public int getmId() {
+        return mId;
+    }
+
+    public static List<Hero> getInstances() {
+        return instances;
+    }
+
+    public String getmWeakness() {
+        return mWeakness;
+    }
+
+    public String getmSpecialPower() {
+        return mSpecialPower;
+    }
+
+    public int getmAge() {
+        return mAge;
+    }
+
+    public Hero(String name, int age, String special_power, String weakness){
         mName=name;
         mAge = age;
         mSpecialPower =special_power;
--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -8,5 +8,28 @@ public class HeroTest {
         Hero myHero =new Hero("Mary",19,"Magician","Lying");
         assertEquals(true,myHero instanceof Hero);
     }
+    @Test
+    public void Hero_instantiatesWithName_String(){
+        Hero myHero =new Hero("Mary",19,"Magician","Lying");
+        assertEquals("Mary",myHero.getmName());
+    }
+    @Test
+    public void Hero_instantiatesWithAge_int(){
+        Hero myHero =new Hero("Mary",19,"Magician","Lying");
+        assertEquals(19,myHero.getmAge());
+    }
+
+    @Test
+    public void Hero_instantiatesWithSpecialPower_String(){
+        Hero myHero =new Hero("Mary",19,"Magician","Lying");
+        assertEquals("Magician",myHero.getmSpecialPower());
+    }
+
+    @Test
+    public void Hero_instantiatesWithWeakness_String() {
+        Hero myHero = new Hero("Mary", 19, "Magician", "Lying");
+        assertEquals("Lying", myHero.getmWeakness());
+    }
+
 
 }
