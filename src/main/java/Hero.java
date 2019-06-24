d91bacb856b5fd4e5994428f714ae36cdaa93735
Rosellyne <elijahroselyne@gmail.com>
2019-06-24 14:27:44 +0300

    created a test  to test the instanceof the constructor

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hero {
+    private String mName;
+    private int mAge;
+    private String mSpecialPower;
+    private String mWeakness;
+    private static List<Hero> instances = new ArrayList<>();
+    private int mId;
+    public Hero(String name,int age,String special_power,String weakness){
+        mName=name;
+        mAge = age;
+        mSpecialPower =special_power;
+        mWeakness = weakness;
+        mId = instances.size();
+    }
 }
--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -1,5 +1,12 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class HeroTest {
+    @Test
+    public void Hero_instantiatesCorrectly_true(){
+        Hero myHero =new Hero("Mary",19,"Magician","Lying");
+        assertEquals(true,myHero instanceof Hero);
+    }
 
 }
